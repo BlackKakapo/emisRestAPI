73d6e05fc6779058c8a417d6d14a41aef38713a8
blackkakapo <petrachialexandru.gmail.com>
2022-01-27 10:45:07 +0200

    first

---
+++ emisRestAPI/src/main/java/com/example/emisRestAPI/EmisRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.emisRestAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmisRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmisRestApiApplication.class, args);
+	}
+
+}
---
+++ emisRestAPI/src/main/java/com/example/emisRestAPI/model/IndexModel.java
@@ -0,0 +1,18 @@
+package com.example.emisRestAPI.model;
+
+public class IndexModel {
+
+    private String responseIndex;
+
+    public IndexModel(String respunseIndex) {
+        this.responseIndex = respunseIndex;
+    }
+
+    public String getResponseIndex() {
+        return responseIndex;
+    }
+
+    public void setResponseIndex(String respunseIndex) {
+        this.responseIndex = respunseIndex;
+    }
+}
