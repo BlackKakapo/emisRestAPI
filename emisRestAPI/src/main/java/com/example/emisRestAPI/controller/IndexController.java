adcc2160259d6fb2ec129728244cb0cf9b62cc8e
blackkakapo <petrachialexandru.gmail.com>
2022-01-27 12:57:57 +0200

    get and post

--- emisRestAPI/src/main/java/com/example/emisRestAPI/controller/IndexController.java
+++ emisRestAPI/src/main/java/com/example/emisRestAPI/controller/IndexController.java
@@ -1,16 +1,38 @@
 package com.example.emisRestAPI.controller;
 
 import com.example.emisRestAPI.model.IndexModel;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Collections;
 
 @RestController
 public class IndexController {
 
+    @Autowired
+    RestTemplate restTemplate;
+
     @GetMapping("/")
-    public IndexModel index(){
-        return new IndexModel("response");
+    public IndexModel[] index(){
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
+        HttpEntity<String> entity = new HttpEntity<>(httpHeaders);
+
+
+        return restTemplate.exchange("https://jsonplaceholder.typicode.com/posts", HttpMethod.GET, entity, IndexModel[].class).getBody();
     }
 
+    @PostMapping("/")
+    public String indexPost(@RequestBody IndexModel indexModel){
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
+        HttpEntity<IndexModel> entity = new HttpEntity<IndexModel>(indexModel, httpHeaders);
 
+        return restTemplate.exchange("https://jsonplaceholder.typicode.com/posts", HttpMethod.POST, entity, String.class).getBody();
+    }
 }
--- emisRestAPI/src/main/java/com/example/emisRestAPI/model/IndexModel.java
+++ emisRestAPI/src/main/java/com/example/emisRestAPI/model/IndexModel.java
@@ -1,18 +1,70 @@
 package com.example.emisRestAPI.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class IndexModel {
 
-    private String responseIndex;
+    @JsonProperty("userId")
+    private Long userId;
 
-    public IndexModel(String respunseIndex) {
-        this.responseIndex = respunseIndex;
+    @JsonProperty("id")
+    private Long id;
+
+    @JsonProperty("title")
+    private String title;
+
+    @JsonProperty("body")
+    private String body;
+
+    public IndexModel() {
     }
 
-    public String getResponseIndex() {
-        return responseIndex;
+    public IndexModel(Long userId, Long id, String title, String body) {
+        this.userId = userId;
+        this.id = id;
+        this.title = title;
+        this.body = body;
     }
 
-    public void setResponseIndex(String respunseIndex) {
-        this.responseIndex = respunseIndex;
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    @Override
+    public String toString() {
+        return "IndexModel{" +
+                "userId=" + userId +
+                ", id=" + id +
+                ", title='" + title + '\'' +
+                ", body='" + body + '\'' +
+                '}';
     }
 }
