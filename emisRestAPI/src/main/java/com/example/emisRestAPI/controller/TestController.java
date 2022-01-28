7f8dc78fa9396bcb6d371d2f4d1518451706aa4c
blackkakapo <petrachialexandru.gmail.com>
2022-01-28 11:01:12 +0200

    2

--- emisRestAPI/src/main/java/com/example/emisRestAPI/controller/TestController.java
+++ emisRestAPI/src/main/java/com/example/emisRestAPI/controller/TestController.java
@@ -4,7 +4,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 public class TestController {
 
-
     @GetMapping("/test")
     public String test(){
         return new String("Test");
